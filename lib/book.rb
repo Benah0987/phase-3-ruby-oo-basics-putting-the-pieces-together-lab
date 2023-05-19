94b187fd17336c9b1002f178ff89fbc979489bf8
Benah0987 <wanyoikebenayah77@gmail.com>
2023-05-19 17:25:45 +0300

    completed

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,29 @@
+class Book
+    attr_accessor :title, :author, :page_count, :genre
+  
+    def initialize(title)
+      @title = title
+    end
+  
+    def turn_page
+      puts "Flipping the page...wow, you read fast!"
+    end
+  end
+
+  # Create a new book object
+book = Book.new("And Then There Were None")
 
 
+puts book.title     
+
+book.author = "Agatha Christie"
+puts book.author     
+
+book.page_count = 272
+puts book.page_count 
+
+book.genre = "Mystery"
+puts book.genre      
+
+
+book.turn_page        
--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,33 @@
 # Make your shoe class here!
+class Shoe
+    attr_accessor :brand, :color, :size, :material, :condition
+  
+    def initialize(brand)
+      @brand = brand
+    end
+  
+    def cobble
+      puts "Your shoe is as good as new!"
+      @condition = "new"
+    end
+  end
+# Create a new shoe object
+shoe = Shoe.new("Nike")
+
+# Access and modify shoe properties
+puts shoe.brand      
+shoe.color = "red"
+puts shoe.color      
+
+shoe.size = 9.5
+puts shoe.size        
+
+shoe.material = "suede"
+puts shoe.material    
+
+shoe.condition = "tattered"
+puts shoe.condition
+
+shoe.cobble           
+puts shoe.condition   
+  
